4b81c172e51f27a834387dfe4b3d3dff961d55ca
jeunebeck <bjhjaswine43@gmail.com>
2024-09-09 20:11:54 +0800

    Create ganjilgenap.cpp

---
+++ ganjilgenap.cpp
@@ -0,0 +1,10 @@
+int main() {
+    int num bilangan;
+std cout << "3";
+std cin >> bilangan;
+    
+if (bilangan % 2==0) {
+    std cout:: << bilangan << "adalah bilangan genap" << std::end1;
+else
+std cout:: << bilangan << "adalah bilangan ganjil" << std::end1;
+}
